93054c24b88ed4f97b1dd64f45db466dc7c13d71
ericabd888 <ericabd888@gmail.com>
2022-04-21 10:17:05 +0800

    Updata NoiseLabelDataset.py

--- NoiseLabelDataset/NoiseLabelDataset.py
+++ NoiseLabelDataset/NoiseLabelDataset.py
@@ -9,14 +9,21 @@ import matplotlib.pyplot as plt
 from .utils import MySubset, train_test_split, Display_img
 classes_name = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 class NoiseLabelDataset(Dataset):
-    def __init__(self, MyData, xtype=None, ytype=None, ErrorRate=0.2, ShowErrorLabel=False, transform=None):   
+    def __init__(self, MyData, xtype=None, ytype=None, ErrorRate=0.2, ShowErrorLabel=False, transform=None):  
+        # Initialize Settings
         self.xtype = xtype
         self.ytype = ytype
         self.ShowErrorL = ShowErrorLabel
         DataLen = len(MyData)
         DataIdx = [i for i in range(DataLen)]
         random.shuffle(DataIdx)
-        self.LabelSet = set(MyData.targets)
+        # Use Set to get random Label
+        if MyData.targets:
+            self.LabelSet = set(MyData.targets)
+        else:
+            self.LabelSet = set()
+            for data in MyData:
+                self.LabelSet.add(data[1])
         self.transform = transform
         ErrorNumber = int(DataLen * ErrorRate)
         self.ErrorIdx = set(DataIdx[:ErrorNumber])
@@ -26,7 +33,7 @@ class NoiseLabelDataset(Dataset):
         x, y = self.MyData[index]
         if self.ShowErrorL:
             ErrorLabel = False
-        if index in self.ErrorIdx:
+        if ErrorRate > 0 and (index in self.ErrorIdx):
             CurrentNum = y
             self.LabelSet.remove(CurrentNum)
             DeleteNum = random.choice(list(self.LabelSet))
